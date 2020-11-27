a1f194ab16232686c3572e259eba3aefb0774a08
JaquelinVega <71902957+JaquelinVega@users.noreply.github.com>
2020-11-26 21:09:22 -0700

    Practica Funciones Predefinidas en Haskell

---
+++ funcional/introduccion/funcionespredefinidas.hs
@@ -0,0 +1,30 @@
+--devuelve el valor absoluto de x
+funcion1 x = abs x
+--menor enterno no menor que x 
+funcion2 x = ceiling x
+--devuelve el valor siguiente de x
+funcion3 x = succ x
+--obtiene el menor
+funcion4 x y = min x y 
+--obtiene el mayor
+funcion5 x y = max x y
+--divide x entre y
+funcion6 x y = div x y
+--obtiene el modulo de x y
+funcion7 x y = mod x y
+--verifica si x es par
+funcion8 x = even x
+--numero mayor entero no  mayor que x
+funcion9 x = floor x
+--máximo común divisor
+funcion10 x y = gcd x y
+--negación de booleana
+funcion11 x = not x
+--verifica si x es numero impar
+funcion12 x = odd x
+--devuelve el valor siguiente de x
+funcion13 x = succ x
+--devuelve 1 si es positivo, -1 si es negativo
+funcion14 x = signum x
+--retorna el segundo elemento. Y en este caso
+funcion15 x y = snd(x,y)
